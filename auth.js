1b49455a8e707a4160720dfce7d712da9ea5039b
Cgr3 <102884458+Cgr3@users.noreply.github.com>
2022-07-15 23:37:47 -0400

    minor auth.js patch
    FIXED auth.js
    Spelling mistakes

--- auth.js
+++ auth.js
@@ -7,10 +7,10 @@ const jwt = require('jsonwebtoken'),
 //Your local passport file
   require('./passport');
 
-let genreateJWTToken = (user) => {
-  return jwt.sing(user, jwtSecret, {
+let generateJWTToken = (user) => {
+  return jwt.sign(user, jwtSecret, {
     subject: user.Username, //This is the username you're encoding in the JWT
-    expiredIn: '7d', //This specifies that the token will expire in 7 days!
+    expiresIn: '7d', //This specifies that the token will expire in 7 days!
     algorithm: 'HS256' //This is the algorithm used to "sign" or encode the values of the JWT
   });
 }
