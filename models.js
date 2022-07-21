a72254225a334ace5293784cadceef1c63c7ba30
Cgr3 <102884458+Cgr3@users.noreply.github.com>
2022-07-20 20:50:25 -0400

    actual models.js minor update
    ACTUALLY fixed models.js

--- models.js
+++ models.js
@@ -29,8 +29,8 @@ userSchema.statics.hashPassword = (password) => {
   return bcrypt.hashSync(password, 10);
 };
 
-userSchema.methods.validatePassword = function(Password) {
-  return bcrypt.compareSync(password, this.password);
+userSchema.methods.validatePassword = function(password) {
+  return bcrypt.compareSync(password, this.Password);
 };
 
 let Movie = mongoose.model('Movie', movieSchema);
