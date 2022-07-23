9ac24ff78b2409415a7aab49b7cbcb814bf3e212
Cgr3 <102884458+Cgr3@users.noreply.github.com>
2022-07-23 11:31:44 -0400

    minor update to index.js
    Trying movies.find for director

--- index.js
+++ index.js
@@ -251,7 +251,7 @@ app.get('/movies/genre/:Name', passport.authenticate('jwt', { session: false }),
 
 //Get director by name
 app.get('/movies/director/:Name', passport.authenticate('jwt', { session: false }), (req, res) => {
-  Movies.findOne({ 'Director.Name': req.params.Name })
+  Movies.find({ 'Director.Name': req.params.Name })
   .then((director) => {
     res.status(201).json(director);
   })
